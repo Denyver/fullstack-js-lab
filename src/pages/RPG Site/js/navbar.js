8dee383675aa16dd4bf4a2dc68eb98f5fd855680
Denyver <deny.fdasilva@hotmail.com>
2024-12-11 14:30:36 -0300

    combat/navbar

---
+++ src/pages/RPG Site/js/navbar.js
@@ -0,0 +1,16 @@
+  let lastScrollTop = 0;
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      // Rolando para baixo
+      navbar.classList.add('hidden');
+    } else {
+      // Rolando para cima
+      navbar.classList.remove('hidden');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Evita valores negativos
+  });
