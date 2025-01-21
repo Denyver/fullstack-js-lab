38fabf71f1082f41010c1e3dbc6126511356dc3d
Denyver <deny.fdasilva@hotmail.com>
2025-01-21 19:58:53 -0300

    toggleMenu

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,27 +1,23 @@
 let lastScrollTop = 0;
-const header = document.querySelector('header'); // Seleciona o cabeçalho
+const header = document.querySelector('header'); 
 
 window.addEventListener('scroll', () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop) {
-    // Rolando para baixo
     header.classList.add('hidden');
   } else {
-    // Rolando para cima
     header.classList.remove('hidden');
   }
 
-  lastScrollTop = Math.max(scrollTop, 0); // Garante que o scrollTop não seja negativo
+  lastScrollTop = Math.max(scrollTop, 0); 
 });
 
 function toggleMenu() {
   const menu = document.querySelector('.menu');
   const toggleButton = document.querySelector('.menu-toggle');
   
-  // Alterna a visibilidade do menu
   menu.classList.toggle('active');
   
-  // Alterna a rotação do ícone de triângulo
   toggleButton.classList.toggle('rotate');
 }
