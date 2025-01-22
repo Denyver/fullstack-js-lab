2de95b2c78b74ab3a2414bd15527b89ffb064b27
Denyver <deny.fdasilva@hotmail.com>
2025-01-21 23:27:00 -0300

    toggleMenu (atualizado)

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,5 +1,7 @@
 let lastScrollTop = 0;
 const header = document.querySelector('header'); 
+const menu = document.querySelector('.menu');
+const toggleButton = document.querySelector('.menu-toggle');
 
 window.addEventListener('scroll', () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -14,10 +16,18 @@ window.addEventListener('scroll', () => {
 });
 
 function toggleMenu() {
-  const menu = document.querySelector('.menu');
-  const toggleButton = document.querySelector('.menu-toggle');
-  
   menu.classList.toggle('active');
-  
   toggleButton.classList.toggle('rotate');
 }
+
+document.addEventListener('click', (event) => {
+  const isClickInsideHeader = header.contains(event.target);
+  const isClickInsideMenu = menu.contains(event.target);
+  const isClickOnToggle = toggleButton.contains(event.target);
+
+  if (!isClickInsideHeader && !isClickInsideMenu && !isClickOnToggle) {
+    header.classList.remove('hidden');
+    menu.classList.remove('active');
+    toggleButton.classList.remove('rotate');
+  }
+});
